48ea53a7f1323091f9f6f90eeb0a8e32adb2a6a3
NikolaZh <djozefs@gmail.com>
2018-04-23 22:12:03 +0300

    mobile explore-zoom fun

--- assets/js/jquery.fs-viewer.js
+++ assets/js/jquery.fs-viewer.js
@@ -173,8 +173,8 @@
                 const imgViewer = viewer.$viewLayer.find('img');
                 const deltaX = touchEndXY.clientX - touchStartXY.clientX;
                 const deltaY = touchEndXY.clientY - touchStartXY.clientY;
-                translateX += deltaX / 25;
-                translateY += deltaY / 25;
+                translateX += deltaX / 5;
+                translateY += deltaY / 5;
                 imgViewer
                     .css({ transform: `translate(${translateX}%,${translateY}%) scale(${scale})` });
                 touchStartXY.clientX = touchEndXY.clientX;
