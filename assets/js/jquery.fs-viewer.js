c2afd18558acf5c37a7481edb1868de58b9043e1
NikolaZh <djozefs@gmail.com>
2018-06-06 23:24:34 +0300

    test mobile handler

--- assets/js/jquery.fs-viewer.js
+++ assets/js/jquery.fs-viewer.js
@@ -62,7 +62,6 @@
             this._context = $context.each(function(i) { // cycle operations with DOM-elements
                 $(this).data('fsViewerIndex', i);
             });
-            // this._deviceIsMobile = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
             this._deviceIsMobile = ('ontouchstart' in document.documentElement);
             this._addUIHandlers();
         }
@@ -116,146 +115,17 @@
         }
 
         _mobileHandler() {
-            const touchStartXY = {};
-            const touchEndXY = {};
-            let touchNumberPrevios = 0;
-            let imgZoomed = false;
-            let scale = 1;
-            let translateX = -50;
-            let translateY = -50;
-            const screenCenterX = $(window).width() / 2;
-            const screenCenterY = $(window).height() / 2;
+            let inSwipe = false;
 
-            const swipeHandler = () => {
-                const deltaX = touchEndXY.clientX - touchStartXY.clientX;
-                const deltaY = touchEndXY.clientY - touchStartXY.clientY;
-                const viewer = this;
-                const imgViewer = viewer.$viewLayer.find('img');
-                if (touchEndXY.touchmove) { // no just one touch and no zoom
-                    if ((Math.abs(deltaX) > Math.abs(deltaY)) && (Math.abs(deltaX) > 50)) { // swipe more horizontal and no short
-                        if (deltaX < 0) {
-                            this._moveToNextImage(true)();
-                        } else if (deltaX > 0) {
-                            this._moveToNextImage(false)();
-                        }
-                    } else if ((Math.abs(deltaX) < Math.abs(deltaY)) && (deltaY < 0) && (Math.abs(deltaY) > 100)) { // vertical swipe from bottom to top and no short
-                        imgViewer
-                            .animate({ top: '25%' }, () => {
-                                this.close();
-                                imgViewer
-                                    .animate({ top: '50%' });
-                            });
-                    }
-                    touchEndXY.touchmove = false;
-                }
-            };
 
-            const zoomHandler = () => {
-                imgZoomed = true;
-                const viewer = this;
-                const imgViewer = viewer.$viewLayer.find('img');
-                const deltaXStart = touchStartXY.clientX2 - touchStartXY.clientX; // coordinates of 2 points at the start of event
-                const deltaYStart = touchStartXY.clientY2 - touchStartXY.clientY;
-                const deltaXEnd = touchEndXY.clientX2 - touchEndXY.clientX; // coordinates of 2 points at the end of event
-                const deltaYEnd = touchEndXY.clientY2 - touchEndXY.clientY;
-                const distanceStart = Math.sqrt(Math.pow(deltaXStart, 2) + Math.pow(deltaYStart, 2));
-                const distanceEnd = Math.sqrt(Math.pow(deltaXEnd, 2) + Math.pow(deltaYEnd, 2));
-                const deltaDistance = (distanceEnd - distanceStart);
-                scale += (deltaDistance / 1000);
-                if (scale <= 1) {
-                    scale = 1;
-                    imgZoomed = false;
-                } else if (scale >= 4) {
-                    scale = 4;
-                }
-                const pinchPoint = {
-                    pinchX: (touchStartXY.clientX + touchStartXY.clientX2) / 2,
-                    pinchY: (touchStartXY.clientY + touchStartXY.clientY2) / 2,
-                }
-                if (deltaDistance > 0 && scale < 4) { // zoom to pinch point
-                    if (pinchPoint.pinchX < screenCenterX) {
-                        translateX += (((screenCenterX - pinchPoint.pinchX) * scale) / 200);
-                    } else {
-                        translateX -= (((pinchPoint.pinchX - screenCenterX) * scale) / 200);
-                    }
-
-                    if (pinchPoint.pinchY < screenCenterY) {
-                        translateY += (((screenCenterY - pinchPoint.pinchY) * scale) / 200);
-                    } else {
-                        translateY -= (((pinchPoint.pinchY - screenCenterY) * scale) / 200);
-                    }
-                }
-
-                if (deltaDistance < 0) { // unzoom
-                    if (!(translateX > -55 && translateX < -45)) {
-                        if (translateX < -50) {
-                            translateX += Math.abs(deltaDistance / 20);
-                        } else {
-                            translateX -= Math.abs(deltaDistance / 20);
-                        }
-                    }
-                    if (!(translateY > -55 && translateY < -45)) {
-                        if (translateY < -50) {
-                            translateY += Math.abs(deltaDistance / 20);
-                        } else {
-                            translateY -= Math.abs(deltaDistance / 20);
-                        }
-                    }
-                }
-
-                if (scale < 1.05) {
-                    translateX = -50;
-                    translateY = -50;
-                }
-
-                imgViewer
-                    .css({ transform: `translate(${translateX}%,${translateY}%) scale(${scale})` });
-            }
-
-            const exploreZoomedImgHandler = () => {
-                const viewer = this;
-                const imgViewer = viewer.$viewLayer.find('img');
-                const deltaX = touchEndXY.clientX - touchStartXY.clientX;
-                const deltaY = touchEndXY.clientY - touchStartXY.clientY;
-                translateX += deltaX / 5;
-                translateY += deltaY / 5;
-                imgViewer
-                    .css({ transform: `translate(${translateX}%,${translateY}%) scale(${scale})` });
-                touchStartXY.clientX = touchEndXY.clientX;
-                touchStartXY.clientY = touchEndXY.clientY;
-            }
 
             return function(e) {
-                e.preventDefault();
-                const numberOfTouches = e.originalEvent.touches.length;
                 const touch = e.originalEvent.touches[0];
-                const touch2 = e.originalEvent.touches[1];
-                if (e.type === 'touchstart') { // combine if 'touchstart' || 'touchmove' don't work. why?
-                    touchStartXY.clientX = touch.clientX;
-                    touchStartXY.clientY = touch.clientY;
-                    if (numberOfTouches === 2) {
-                        touchStartXY.clientX2 = touch2.clientX;
-                        touchStartXY.clientY2 = touch2.clientY;
-                    }
+                if (e.type === 'touchstart' && e.originalEvent.touches.length === 1) {
+                    inSwipe = true;
                 }
-                if (e.type === 'touchmove') {
-                    touchEndXY.touchmove = true;
-                    touchEndXY.clientX = touch.clientX;
-                    touchEndXY.clientY = touch.clientY;
-                    touchNumberPrevios = 1;
-                    if (imgZoomed && numberOfTouches === 1) {
-                        exploreZoomedImgHandler();
-                    }
-                    if (numberOfTouches === 2) {
-                        touchEndXY.clientX2 = touch2.clientX;
-                        touchEndXY.clientY2 = touch2.clientY;
-                        touchNumberPrevios = 2;
-                        zoomHandler();
-                    }
-                }
-                if (e.type === 'touchend' && !imgZoomed && touchNumberPrevios === 1) {
-                    swipeHandler();
-                }
+                console.log(inSwipe);
+
             };
         }
 
